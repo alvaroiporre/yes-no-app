90edcf7156f67ffbf047451eb807996c999790be
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-12 21:45:40 -0400

    Add messageFielsBox

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app1/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app1/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app1/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -39,7 +40,8 @@ class _ChatView extends StatelessWidget {
                   ? const MyMessageBubble()
                   : const HerMessageBubble();
             })),
-            const Text('mundo')
+            // text box of immage
+            const MessageFieldBox(),
           ],
         ),
       ),
---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final outlineInputBorder = UnderlineInputBorder(
+        borderSide: const BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(40));
+
+    final inputDecoration = InputDecoration(
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {},
+      ),
+    );
+    return TextFormField(
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Submit value $value');
+      },
+      onChanged: (value) {
+        print('Change value $value');
+      },
+    );
+  }
+}
