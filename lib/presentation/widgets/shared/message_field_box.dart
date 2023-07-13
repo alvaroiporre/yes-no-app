abb364ce970aa7fe1adfb6e7bb5100b1d0f184c3
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-12 21:59:14 -0400

    Add text form events

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,26 +5,37 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
       suffixIcon: IconButton(
         icon: const Icon(Icons.send_outlined),
-        onPressed: () {},
+        onPressed: () {
+          final textValue = textController.value.text;
+          print('Button: $textValue');
+          textController.clear();
+        },
       ),
     );
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
-        print('Submit value $value');
-      },
-      onChanged: (value) {
-        print('Change value $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
     );
   }
