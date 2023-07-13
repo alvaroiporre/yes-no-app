1c5a0d79cde190efb2ee92a9f5651703477dfcdd
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-12 20:34:26 -0400

    Add body

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/alvaroalvaroiporremartinez/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/alvaroalvaroiporremartinez/Desktop/Flutter/projects/yes_no_app1"
+export "FLUTTER_APPLICATION_PATH=/Users/alvaroalvaroiporremartinez/Desktop/Flutter/projects/yes_no_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/alvaroalvaroiporremartinez/Desktop/Flutter/projects/yes_no_app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iNGZiMTEyMTRkZDJkZGE2Y2UwMTJkZDk4ZWE0OThlOWU4YjkxMjYyLw=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/alvaroalvaroiporremartinez/Desktop/Flutter/projects/yes_no_app/.dart_tool/package_config.json"
--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -10,11 +10,34 @@ class ChatScreen extends StatelessWidget {
         leading: const Padding(
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
-            backgroundImage: NetworkImage('https://media.glamour.es/photos/616f8e37d189dd4d546b213a/master/pass/631589.jpeg'),
+            backgroundImage: NetworkImage(
+                'https://media.glamour.es/photos/616f8e37d189dd4d546b213a/master/pass/631589.jpeg'),
           ),
         ),
         title: const Text('Mi amor'),
         centerTitle: true,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice $index');
+            })),
+            const Text('mundo')
+          ],
+        ),
       ),
     );
   }
